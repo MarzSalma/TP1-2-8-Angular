e2c18a03e19b941ef7477d45d4ca5df1088d9c0b
MarzSalma <merzougsalma@nabeul.r-iset.tn>
2024-10-03 22:33:08 +0100

    adding

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-produits',
-  templateUrl: './produits.component.html',
+  templateUrl: './produits.component.html'
 })
 export class ProduitsComponent implements OnInit {
-  produits: string[];
+  produits? : Produit[]
+  private produitService= new ProduitService()
 
-  constructor() {
-    this.produits = ['PC Asus', 'Imprimante Epson', 'Tablette Samsung'];
+  constructor( ) {
+    //this.produits=[]
+    }
+  ngOnInit(): void {
+    this.produits = this.produitService.listeProduits();
+
   }
-  ngOnInit(): void {}
 }
